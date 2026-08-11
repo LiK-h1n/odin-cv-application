4bf7457ba9c78cdb2be5380f3b9de6ead5bd7898
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-08-11 13:52:42 +0530

    Create component architecture

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,5 @@
+function GeneralInfo() {
+  return <></>;
+}
+
+export default GeneralInfo;
---
+++ src/components/PracticalExperience.jsx
@@ -0,0 +1,5 @@
+function PracticalExperience() {
+  return <></>;
+}
+
+export default PracticalExperience;
