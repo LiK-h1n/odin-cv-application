926e5d48115930b03dc35faf09eef5bc9d7c92be
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-08-11 16:23:35 +0530

    Add components into App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import GeneralInfo from "./components/GeneralInfo";
 import Education from "./components/Education";
+import PracticalExperience from "./components/PracticalExperience";
 
 function App() {
   return (
     <>
-      <GeneralInfo></GeneralInfo>
-      <Education></Education>
+      <h1>Resume Flow</h1>
+      <GeneralInfo />
+      <Education />
+      <PracticalExperience />
     </>
   );
 }
