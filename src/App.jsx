8aa12410cc4766a2bf100abaab41d57483c51873
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-08-11 15:31:22 +0530

    Implement Education.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import GeneralInfo from "./components/GeneralInfo";
+import Education from "./components/Education";
 
 function App() {
-  return <GeneralInfo></GeneralInfo>;
+  return (
+    <>
+      <GeneralInfo></GeneralInfo>
+      <Education></Education>
+    </>
+  );
 }
 
 export default App;
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
+
 function Education() {
-  return <></>;
+  const [schoolName, setSchoolName] = useState("");
+  const [titleOfStudy, setTitleOfStudy] = useState("");
+  const [dateOfStudy, setDateOfStudy] = useState("");
+  const [isEditing, setIsEditing] = useState(true);
+
+  function handleSubmitClick(event) {
+    event.preventDefault();
+    setIsEditing(false);
+  }
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  return (
+    <section class="education-info">
+      {isEditing ? (
+        <>
+          <h2>Education</h2>
+          <form>
+            <div class="form-group">
+              <label>School Name:</label>
+              <input
+                type="text"
+                value={schoolName}
+                onChange={(event) => setSchoolName(event.target.value)}
+              />
+            </div>
+
+            <div class="form-group">
+              <label>Title of Study:</label>
+              <input
+                type="text"
+                value={titleOfStudy}
+                onChange={(event) => setTitleOfStudy(event.target.value)}
+              />
+            </div>
+
+            <div class="form-group">
+              <label>Date of Study:</label>
+              <input
+                type="text"
+                value={dateOfStudy}
+                onChange={(event) => setDateOfStudy(event.target.value)}
+              />
+            </div>
+
+            <button type="submit" onClick={handleSubmitClick}>
+              Submit
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h2>Education</h2>
+          <div class="info-display">
+            <p>
+              <strong>School:</strong> {schoolName}
+            </p>
+            <p>
+              <strong>Degree:</strong> {titleOfStudy}
+            </p>
+            <p>
+              <strong>Date:</strong> {dateOfStudy}
+            </p>
+            <button type="button" onClick={handleEditClick}>
+              Edit
+            </button>
+          </div>
+        </>
+      )}
+    </section>
+  );
 }
 
 export default Education;
